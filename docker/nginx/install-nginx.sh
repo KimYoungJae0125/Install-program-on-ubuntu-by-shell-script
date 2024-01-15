33a86dcf9084b90d52adb3aff4098261cf37580a
kyj <>
2024-01-15 23:18:16 +0900

    Modify Nginx Config

--- docker/nginx/install-nginx.sh
+++ docker/nginx/install-nginx.sh
@@ -22,10 +22,15 @@ if [ -n $NGINX_CONTAINER_ID ];then
 	sudo docker stop $NGINX_CONTAINER_ID
 	sudo docker rm $NGINX_CONTAINER_ID
 fi
+
+sudo cp -r ./conf.d /var/nginx
+sudo sed -i 's/$GITLAB_DOMAIN/{{Domain to be replaced}}/g' /var/nginx/conf.d/gitlab.conf
+
 sudo docker run -it \
 		--name nginx-docker \
 		-d \
-		-p 80:80 \
+		-p 80:80 -p 443:443 \
 		--add-host=host.docker.internal:host-gateway \
+		-v /var/nginx/conf.d:/etc/nginx/conf.d \
+		-v /var/nginx/cert:/var/cert \
 		nginx
-
