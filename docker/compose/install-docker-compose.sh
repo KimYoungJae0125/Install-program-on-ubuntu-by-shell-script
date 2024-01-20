236b17dc17d7bc6021f04b09c71034b9eb575a4c
kyj <>
2024-01-20 22:57:52 +0900

    Add Docker-compose

---
+++ docker/compose/install-docker-compose.sh
@@ -0,0 +1,7 @@
+## https://github.com/docker/compose/releases에서 버전 참고
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose -v
